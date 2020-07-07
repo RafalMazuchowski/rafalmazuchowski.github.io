a112024a2785a138dca87f856976f786f206270b
RafalMazuchowski <mazuchowski.r@gmail.com>
2020-07-07 12:56:09 +0200

    Initial commit 3.13

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://mysterious-mesa-46681.herokuapp.com/v1/task';
+  const apiRoot = 'https://mysterious-mesa-46681.herokuapp.com/v1/';
   const trelloApiRoot = 'https://mysterious-mesa-46681.herokuapp.com/v1/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
